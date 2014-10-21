549d8a55f15e1ec35fb8a69c32d97a13ae65f9b3
sparkyd168 <sparkyd168@gmail.com>
2014-10-21 11:42:28 +0530

    First try
    working login.

---
+++ app/src/main/java/sparkyd/bloodbankcet/Home.java
@@ -0,0 +1,80 @@
+package sparkyd.bloodbankcet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class Home extends Activity {
+int logged_in=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        try{
+            Intent logged = this.getIntent();
+            if (logged!=null){
+                logged_in=getIntent().getExtras().getInt("logged");
+            }
+        }
+        catch (Exception e){
+
+        }
+
+        if(logged_in==1)
+            invalidateOptionsMenu();
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        if (logged_in==1) {
+
+            MenuItem loginn=menu.findItem(R.id.action_login);
+            loginn.setVisible(false);
+
+
+        }
+
+        if (logged_in==0) {
+
+            MenuItem logout=menu.findItem(R.id.action_logout);
+            logout.setVisible(false);
+//            MenuItem admin=menu.findItem(R.id.action_admin);
+//            admin.setVisible(false);
+
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Toast.makeText(Home.this,"Not implemented yet",Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.action_login) {
+            Intent login =new Intent(Home.this,LoginPage.class);
+            startActivity(login);
+            //finish();
+        }
+        if (id == R.id.action_logout){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/sparkyd/bloodbankcet/LoginPage.java
@@ -0,0 +1,77 @@
+package sparkyd.bloodbankcet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import sparkyd.bloodbankcet.R;
+
+public class LoginPage extends Activity {
+    EditText username,password;
+    Button action_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        action_login=(Button)findViewById(R.id.button_login);
+
+
+        action_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int log=0;
+                String user=username.getText().toString();
+                String pass=password.getText().toString();
+
+                String login_user="Nisham";
+                String login_pass="pass";
+
+                if(user.equals(login_user) && pass.equals(login_pass)){
+                    Toast.makeText(LoginPage.this,"Success",Toast.LENGTH_SHORT).show();
+                    log=1;
+                }
+
+                if(log==1){
+                    Intent logged_in=new Intent(LoginPage.this,Home.class);
+                    logged_in.putExtra("logged",log);
+                    startActivity(logged_in);
+                    finish();
+
+                }else{
+                    password.setText("");
+                    Toast.makeText(LoginPage.this,"Username or Password incorrect",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Toast.makeText(LoginPage.this,"Not implemented yet",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
