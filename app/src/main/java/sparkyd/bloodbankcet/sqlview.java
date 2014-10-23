2d64384854f5832cd62f744127660da2e41de771
moonblade <moonblade168@gmail.com>
2014-10-23 18:10:21 +0530

    minor

--- app/src/main/java/sparkyd/bloodbankcet/sqlview.java
+++ app/src/main/java/sparkyd/bloodbankcet/sqlview.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
+import moonblade.bloodbankcet.sqldb;
 import moonblade.bloodbankcet.R;
 
 public class sqlview extends Activity {
